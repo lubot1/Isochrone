2e6b115e991926820c625ff161256fcc7c395217
Luis Botello <botello48@gmailcom>
2021-04-24 11:36:56 -0400

    Fixed map script path

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include "./Views/Components/header.php";
 
 ?>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1iQJm4S8GybM8MysZdPXvERLs6cFpCtk&callback=initMap&libraries=geometry" defer></script>
-<script src="./Libraries/js/maps.js"></script>
+<script src="./js/maps.js"></script>
 <link rel="stylesheet" href="map.css">
 
 <h1>Test</h1>
