59c4bff7d05ab89ebf213b5b1b1b6e132530b836
Luis Botello <botello48@gmailcom>
2021-04-24 11:38:09 -0400

    Fixed stylesheet path

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include "./Views/Components/header.php";
 ?>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1iQJm4S8GybM8MysZdPXvERLs6cFpCtk&callback=initMap&libraries=geometry" defer></script>
 <script src="./js/maps.js"></script>
-<link rel="stylesheet" href="map.css">
+<link rel="stylesheet" href="./Stylesheets/map.css">
 
 <h1>Test</h1>
 <div id="map"></div>
