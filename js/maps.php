67ccdd3f38fba57545f7aec0ed26dc1fb09c3669
Luis Botello <botello48@gmailcom>
2021-04-25 14:14:15 -0400

    Added shape to map hopefully

--- js/maps.php
+++ js/maps.php
@@ -8,12 +8,21 @@ $data = $isoBounds->getIsoPoints();
 <script>
 function initMap() {
   var data = <?= $data ?>;
-  console.log(data);
+  const isoShell = data.results[0].shapes[0].shell;
   //Initialize new map object
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 0, lng: 0 },
-    zoom: 1,
+    center: { lat: 43.64538993070304, lng: -79.38089475089429 },
+    zoom: 4,
   });
+  const isoBorders = new google.maps.Polygon({
+    paths: isoShell,
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+  });
+  isoBorders.setMap(map);
   //Created using the events sample at https://developers.google.com/maps/documentation/javascript/events#maps_event_simple-javascript
   // map.addListener("click", (e) => {
   //   new google.maps.Marker({
