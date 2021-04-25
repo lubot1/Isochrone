49b363cf83a7d2b4d007088d82424d82ee2ad49b
Luis Botello <botello48@gmailcom>
2021-04-25 13:12:20 -0400

    Changed order of script files

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 include "./Views/Components/header.php";
 ?>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1iQJm4S8GybM8MysZdPXvERLs6cFpCtk&callback=initMap&libraries=geometry" defer></script>
 <?php include "./js/maps.php"; ?>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1iQJm4S8GybM8MysZdPXvERLs6cFpCtk&callback=initMap&libraries=geometry" defer></script>
+
 <link rel="stylesheet" href="./Stylesheets/map.css">
 
 <h1>Test</h1>
