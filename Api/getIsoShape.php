68be6688fdf46a93a89ee162528c994e74f8177f
Luis Botello <botello48@gmailcom>
2021-04-26 11:43:17 -0400

    Added debug code to api

--- Api/getIsoShape.php
+++ Api/getIsoShape.php
@@ -9,12 +9,12 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $data = json_decode(file_get_contents("php://input"));
-var_dump($data);
+
 if (!empty($data->coords) && !empty($data->selectedTime)) {
     $isoBounds = new DistanceTimeRequest($data->coords,$data->selectedTime);
     $dataOut = $isoBounds->getIsoPoints();
     $dataOut = json_decode($dataOut);
-
+    var_dump($dataOut);
     if (isset($dataOut->results)) {
         http_response_code(200);
         echo(json_encode($dataOut->results[0]->shapes));
