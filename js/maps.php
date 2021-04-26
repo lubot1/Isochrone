77715f8bfc49cfeffb8bf3061e19151a453f0ca5
Luis Botello <botello48@gmailcom>
2021-04-26 12:07:29 -0400

    Changed zoom distance on map

--- js/maps.php
+++ js/maps.php
@@ -10,14 +10,13 @@ $data = $isoBounds->getIsoPoints();
 
 <script>
 function initMap() {
-  // TODO: Add an api to serve JSON data of shells and holes on click
   var data = <?= $data ?>;
   console.log(data);
   const isoShell = data.results[0].shapes[0].shell;
   //Initialize new map object
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 43.64538993070304, lng: -79.38089475089429 },
-    zoom: 6,
+    zoom: 8,
   });
   const isoBorders = new google.maps.Polygon({
     paths: isoShell,
