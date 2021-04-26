fec7079fd15d0dd66f99727732a16002a72186ac
Luis Botello <botello48@gmailcom>
2021-04-26 11:21:42 -0400

    Added error checking

--- Api/getIsoShape.php
+++ Api/getIsoShape.php
@@ -1,5 +1,6 @@
 <?php
 require_once "../Models/isochrone.php";
+include "../config.php";
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -26,5 +27,6 @@ if (!empty($data->coords) && !empty($data->selectedTime)) {
 else {
     http_response_code(400);
     echo(json_encode(array("message"=>"Missing data")));
+    echo(json_encode())
 }
 ?>
