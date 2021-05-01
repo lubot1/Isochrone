d67e57ae41904450ac0dd66497a1749f4fee8f6b
Luis Botello <botello48@gmailcom>
2021-05-01 14:16:24 -0400

    Added shape drawing

--- js/maps.js
+++ js/maps.js
@@ -1,4 +1,6 @@
 function initMap() {
+  var holesArray = [];
+  var shellArray = [];
   // var data = $data;
   map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: 43.64538993070304, lng: -79.38089475089429 },
@@ -6,28 +8,59 @@ function initMap() {
     });
 
     map.addListener("click", (res) => {
+      holesArray.forEach(element => {
+        element.setMap(null);
+      });
+      shellArray.forEach(element => {
+        element.setMap(null);
+      });
+      
       var currTime = new Date(Date.now());
       var location = res.latLng.toJSON();
       var data = {
         coords: location,
         selectedTime: currTime.toISOString()
       };
-      console.log(data);
       $.post("../Api/getIsoShape.php",JSON.stringify(data),function (response, status) {
+        //response is an array of shapes to draw on the map
+        response.forEach(shapeObject => {
+
+          var hole = new google.maps.Polygon({
+            paths: shapeObject.hole,
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0,
+            map: map,
+          });
+          holesArray.push(hole);
+
+          var shell = new google.maps.Polygon({
+            paths: shapeObject.shell,
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.35,
+            map: map,
+          });
+          shellArray.push(hole);
+        });
         console.log(response);
       });
       map.panTo(res.latLng);
     });
 
-  // const isoShell = data.results[0].shapes[0].shell;
-  // //Initialize new map object
-  // const isoBorders = new google.maps.Polygon({
-  //   paths: isoShell,
-  //   strokeColor: "#FF0000",
-  //   strokeOpacity: 0.8,
-  //   strokeWeight: 2,
-  //   fillColor: "#FF0000",
-  //   fillOpacity: 0.35,
-  // });
-  // isoBorders.setMap(map);
+  const isoShell = data.results[0].shapes[0].shell;
+  //Initialize new map object
+  const isoBorders = new google.maps.Polygon({
+    paths: isoShell,
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+  });
+  isoBorders.setMap(map);
 }
