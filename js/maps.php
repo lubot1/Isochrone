1500e2a2a05899c6304ca7a29ae7f7ab79159e39
Luis Botello <botello48@gmailcom>
2021-05-01 13:20:03 -0400

    Added event to look for coordinates on a click

--- js/maps.php
+++ js/maps.php
@@ -1,31 +1,39 @@
 <?php 
 require_once "./Models/isochrone.php";
-$coords->lat = 43.64538993070304;
-$coords->lng = -79.38089475089429;
-$today = "\"".date("c")."\"";
+// $coords->lat = 43.64538993070304;
+// $coords->lng = -79.38089475089429;
+// $today = "\"".date("c")."\"";
 
-$isoBounds = new DistanceTimeRequest($coords,$today);
-$data = $isoBounds->getIsoPoints();
+//$isoBounds = new DistanceTimeRequest($coords,$today);
+//$data = $isoBounds->getIsoPoints();
 ?>
 
 <script>
 function initMap() {
-  var data = <?= $data ?>;
-  console.log(data);
-  const isoShell = data.results[0].shapes[0].shell;
-  //Initialize new map object
+  var data;
+  // var data = <?= $data ?>;
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 43.64538993070304, lng: -79.38089475089429 },
-    zoom: 8,
-  });
-  const isoBorders = new google.maps.Polygon({
-    paths: isoShell,
-    strokeColor: "#FF0000",
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: "#FF0000",
-    fillOpacity: 0.35,
-  });
-  isoBorders.setMap(map);
+      center: { lat: 43.64538993070304, lng: -79.38089475089429 },
+      zoom: 8,
+    });
+
+    map.addListener("click", (res) => {
+      data = res.latLng;
+      map.panTo(res.latLng);
+      map.setZoom(8);
+    });
+  console.log(data);
+
+  // const isoShell = data.results[0].shapes[0].shell;
+  // //Initialize new map object
+  // const isoBorders = new google.maps.Polygon({
+  //   paths: isoShell,
+  //   strokeColor: "#FF0000",
+  //   strokeOpacity: 0.8,
+  //   strokeWeight: 2,
+  //   fillColor: "#FF0000",
+  //   fillOpacity: 0.35,
+  // });
+  // isoBorders.setMap(map);
 }
 </script>
