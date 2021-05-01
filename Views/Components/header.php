6196a65c2713548c8fc07732f7aaebacb7e26a83
Luis Botello <botello48@gmailcom>
2021-05-01 19:24:48 -0400

    Added design and integrated form

--- Views/Components/header.php
+++ Views/Components/header.php
@@ -10,21 +10,18 @@ include "../../config.php";
     <title>Isochrone</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./Stylesheets/style.css">
 </head>
-<body>
+<body class="d-flex flex-column justify-content-center align-items-center">
     <div class="container">
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
             <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
             <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
             <span class="fs-4">Isochrone</span>
             </a>
-
             <ul class="nav nav-pills">
-            <li class="nav-item"><a href="#" class="nav-link active">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+                <li class="nav-item"><a href="#map" class="nav-link active">Home</a></li>
+                <li class="nav-item"><a href="#features" class="nav-link">Features</a></li>
             </ul>
         </header>
     </div>
--- js/maps.js
+++ js/maps.js
@@ -1,32 +1,53 @@
 function initMap() {
-  var newLayer
+  var data;
+  var newLayer;
+  var city;
+  var mode;
+  var range;
+
+  var control = document.forms.controls;
+  var cityInput = control.city;
+  var rangeInput = control.range;
+  var modeInput = control.mode;
+  var submit = control.submit;
 
   map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: 43.64538993070304, lng: -79.38089475089429 },
-      zoom: 8,
+    center: { lat: 43.64538993070304, lng: -79.38089475089429 },
+    zoom: 8,
+  });
+
+  control.onsubmit = function() {
+    city = cityInput.value;
+    mode = modeInput.value;
+    range = rangeInput.value;
+    
+    data = {
+      mode: mode,
+      timeRange: range,
+    };
+
+    return false
+  };
+
+  map.addListener("click", (res) => {
+    map.data.forEach((feature) => {
+      map.data.remove(feature);
     });
+    var location = res.latLng.toJSON();
 
-    map.addListener("click", (res) => {
-      map.data.forEach((feature) => {
-        map.data.remove(feature);
+    data.coordinates = location; 
+    console.log(data);
+    $.post("../Api/getIsoShape.php",JSON.stringify(data),function (response, status) {
+      //response is an array of shapes to draw on the map
+      newLayer = map.data.addGeoJson(response);
+      map.data.setStyle({
+        fillColor: 'blue',
+        strokeWeight: 1,
       });
-      var location = res.latLng.toJSON();
+      console.log(map.data);
 
-      var data = {
-        coordinates: location,
-        mode: "approximated_transit",
-        timeRange: 1800,
-      };
-      $.post("../Api/getIsoShape.php",JSON.stringify(data),function (response, status) {
-        //response is an array of shapes to draw on the map
-        newLayer = map.data.addGeoJson(response);
-        map.data.setStyle({
-          fillColor: 'blue',
-          strokeWeight: 1,
-        });
-        console.log(map.data);
-
-      });
-      map.panTo(res.latLng);
     });
+    map.panTo(res.latLng);
+  });
+
 }
